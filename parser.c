1ccec7dde2b9f4f6872ceee889ca8403ee5e915d
Jacob <yakovfumm@gmail.com>
2025-05-19 16:15:34 +0300

    now fr

--- parser.c
+++ parser.c
@@ -1,55 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<ctype.h>
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
 #define T_NUMBER 0
 #define T_OPERATOR 1
 #define T_BRACKET 2
-
-typedef struct Shape
-{
-    char Rule[100];
-}Shape;
+#define T_VARIABLE 3
+#define T_EQUALITY 4
 
 typedef struct token
 {
     int value;
     int type; // 0 - number , 1 - opertaion, 2 - brackets
 }token;
+
 typedef struct point
 {
     float x;
     float y;
+    float z;
 }point;
 
-float parseShape(Shape*, point*);
-float atoi_Conversion(const char* strg);
-void evaluate(float* Result, char operator);
 void printScreen(char screen[][100], int length, int width);
 void chooseChar(char* p, float dist);
-
+int parseRule(char* rule, token** tokens);
+void printTokens(token* tokens, int size);
 
 int main()
 {
-    Shape s = {"x^2 + y <= 10"};
-    char screen[100][100] = {0};
+    char* str = "x^2 + y^2 >= 16";
+    token* tokens;
+    int size = parseRule(str, &tokens);
+    printTokens(tokens, size);
+}
 
-    int i, j;
-    for(int i = 0; i < 100; i++)
+void printTokens(token* tokens, int size)
+{
+    int i = 0;
+    for(; i< size; i++)
     {
-        for(int j = 0; j < 100; j++)
-        {
-           /* point p = {i-50,0.5*j-25};
-            if(parseShape(&s, &p) > 0)
-            {
-                screen[i][j] = '@';
-            }
-            else screen[i][j] = ' ';
-            //chooseChar(&screen[i][j], parseShape(&s, &p));*/
-        }
+        printf("Type: %d , val: %d\n", tokens[i].type, tokens[i].value);
     }
-    printScreen(screen, 100, 100);
 }
 
 void chooseChar(char* p, float dist)
@@ -63,7 +56,6 @@ void chooseChar(char* p, float dist)
     }
     *p = map[abs((int)dist*20)];
 }
-
 
 void printScreen(char screen[][100], int length, int width)
 {
@@ -79,87 +71,57 @@ void printScreen(char screen[][100], int length, int width)
     }
 }
 
+int isPointOnShape(token* tokens, int size, point p)
+{
+    
+}
 
-
-
-
-
-
-/*float parseShape(Shape *s, point *p)
-{//takes ruleset from s and parses the symbols to get if point p is on the shape
-    int i;
-    char str[100];
-    strcpy(str,s->Rule);
-    char operator;
-    float result[4] , size = 4;
-    for(i = 0; i < strlen(s->Rule); i++)
+int parseRule(char* rule, token** res)
+{//Takes equation in str and tokenizes to numbers, operators, brackets, order signs, variables
+    token* tokens = (token*)malloc(sizeof(token));
+    token* temp;
+    int i = 0, index_t = 0;
+    while(rule[i] != '\0')
     {
-        switch(str[i])
+        while(rule[i] == ' ') i++;
+        if(isdigit(rule[i]))
         {
-            case 'x':
-                result[0] = p->x;
-                break;
-            case 'y':
-                result[1] = p->y;
-                break;
-            case '^':
-                if(str[i-1] == 'x')
-                {
-                    result[0] = (int)pow(result[0],atoi_Conversion(&str[i+1]));
-                    i++;
-                }
-                if(str[i-1] == 'y')
-                {
-                    result[1] = (int)pow(result[1],atoi_Conversion(&str[i+1]));
-                    i++;
-                }
-                break;
-            case '+':
-                operator = '+';
-                break;
-            case '=':
-                evaluate(result, operator);
-                return result[2] == atoi_Conversion(&str[i+2]);
-            case '<':
-                evaluate(result, operator);
-                if(str[i+1] == '=')
-                {
-                    return result[2] <= atoi_Conversion(&str[i+3]);
-                }
-                return result[2] < atoi_Conversion(&str[i+2]);
-            case '>':
-                evaluate(result, operator);
-                if(str[i+1] == '=')
-                {
-                    return result[2] >= atoi_Conversion(&str[i+3]);
-                }  
-                return result[2] < atoi_Conversion(&str[i+2]);
+            tokens[index_t].type = T_NUMBER;
+            tokens[index_t].value = 0;
+            do
+            {
+                tokens[index_t].value = tokens[index_t].value * 10 + (rule[i] - '0');
+                i++;
+            } while (isdigit(rule[i]));
         }
+        else if(strchr("+\\-*^",rule[i]) != NULL)
+        {
+            tokens[index_t].type = T_OPERATOR;
+            tokens[index_t].value = rule[i];
+            i++;
+        }
+        else if(strchr("()",rule[i]) != NULL)
+        {
+            tokens[index_t].type = T_BRACKET;
+            tokens[index_t].value = rule[i];
+            i++;
+        }
+        else if(strchr("xyz",rule[i]) != NULL)
+        {
+            tokens[index_t].type = T_VARIABLE;
+            tokens[index_t].value = rule[i];
+            i++;
+        }
+        else if(strchr("=><",rule[i]) != NULL)
+        {
+            tokens[index_t].type = T_EQUALITY;
+            tokens[index_t].value = rule[i];
+            i++;
+        }
+        temp = (token*)realloc(tokens, (index_t + 2)*sizeof(token));
+        tokens = temp;
+        index_t++;
     }
-
+    *res = tokens;
+    return (index_t + 1);
 }
-
-void evaluate(float* Result, char operator)
-{
-    switch(operator)
-    {
-        case '+':
-            Result[2] = Result[0] + Result[1];
-            break;
-    }
-}
-
-float atoi_Conversion(const char* strg)
-{
-
-    // Initialize res to 0
-    int res = 0;
-    int i = 0;
-
-    // Iterate through the string strg and compute res
-    while (strg[i] != ' ' && strg[i] != '\0' ) {
-        res = res * 10 + (strg[i] - '0');
-        i++;
-    }
-    return res;
-}*/
