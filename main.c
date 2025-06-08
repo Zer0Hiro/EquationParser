80ac33d104ae8f3cf98ef09a1c9db72de1a7f3ef
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-06-08 18:34:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,25 @@
 #define WIDTH 100
 #define HEIGHT 40
 
+typedef struct rgb
+{
+    int r;
+    int g;
+    int b;
+} RGB;
+
 // Size of cube
 const float cWidth = 40;
 
-//EQUATION
-char* str = "abs((x^2+y^2)^(1/2) - 25) <= 100";
-
+// EQUATION
+char *str = "x^2 + y^2 <= z^2";
 
 // Buffers for output points
 char buffer[WIDTH * HEIGHT];
 // Coordinates of projection z-buffering
 float zbuffer[WIDTH * HEIGHT];
 // Buffer for color
-char *colorbuffer[WIDTH * HEIGHT];
+RGB colorbuffer[WIDTH * HEIGHT];
 
 int background = ' ';
 int xproj, yproj; // projection position
@@ -26,29 +32,27 @@ float ooz;        // z-buffer 1/z
 float x, y, z; // Point coords
 float A, B, C; // Angle
 
-// Calculate coordinates of a point
 void calculatePoint(int i, int j, int k, int sym, char *color);
-
-char map(float ooz);
-
+char map(float a);
+RGB ColorMap(float ooz);
 
 int main()
 {
     float i, j, g;
     int k;
-    char *lastcolor, *newcolor;
+    int lastcolor, newcolor;
+    int maxR = 0;
 
     system("cls");
 
-
-    token* tokens;
+    token *tokens;
     int size = parseRule(str, &tokens);
     tokenQueue queue;
     initialize_queue(&queue);
     convertToPolish(&queue, tokens, size);
 
     free(tokens);
-    
+
     while (1)
     {
         memset(buffer, background, sizeof(buffer));
@@ -58,12 +62,12 @@ int main()
         {
             for (j = -cWidth / 2; j < cWidth / 2; j += 1)
             {
-                for(g = -cWidth /2; g <= cWidth /2; g+= 1)
+                for (g = -cWidth / 2; g <= cWidth / 2; g += 1)
                 {
-                    point p = {i,j,g};
-                    if(evalPolish(&queue, p))
+                    point p = {i, j, g};
+                    if (evalPolish(&queue, p))
                     {
-                        calculatePoint(i, j, g, '@', "\033[33m");  // Z fixed
+                        calculatePoint(i, j, g, '@', "\033[33m"); // Z fixed
                     }
                     // calculatePoint(i, j, -g, '#', "\033[32m"); // -Z fixed
                     // calculatePoint(g, i, j, '%', "\033[33m");  // X fixed
@@ -77,34 +81,39 @@ int main()
         // Move cursor to home position
         printf("%c[%d;%df", 0x1B, 0, 0);
 
-        // Coordinates of X Y Z
-        //printf("%0.2f %0.2f %0.2f\n", x, y, z);
+        // Coordinates of X Y Z (Debug)
+        // printf("%0.2f %0.2f %0.2f\n", x, y, z);
 
         // Print all points
         for (k = 0; k < WIDTH * HEIGHT; k++)
         {
             // Color swap
-            // newcolor = (buffer[k] != ' ') ? colorbuffer[k] : "\033[0m";
-            // if (strcmp(lastcolor, newcolor) != 0)
-            // {
-            //     lastcolor = newcolor;
-            //     printf("%s", newcolor);
-            // }
+            newcolor = (buffer[k] != ' ') ? 1 : 0;
+            if (newcolor != lastcolor)
+            {
+                lastcolor = newcolor;
+                printf("\033[38;2;%d;%d;%dm", colorbuffer[k].r, colorbuffer[k].g, colorbuffer[k].b);
+            }
 
             // Print Char
             putchar(k % WIDTH ? buffer[k] : '\n');
         }
 
+        if (C == 1)
+            C = 0;
+        if (B == 1)
+            B = 0;
+        if (A == 1)
+            A = 0;
         C += 0.05; // X rotation (roll)
         B += 0.05; // Y rotation (pitch)
         A += 0.05; // Z rotation (yaw)
+
         Sleep(10);
     }
 }
 
-// TO DO
-// REWORK THIS PART AS ONE STRUCT WITH ONE MATRIX
-
+// Calculate position of the point
 void calculatePoint(int i, int j, int k, int sym, char *color)
 {
 
@@ -132,7 +141,7 @@ void calculatePoint(int i, int j, int k, int sym, char *color)
     y = y3;
     z = z3 + cam;
     ooz = 1 / z;
-    
+
     xproj = (int)(WIDTH / 2 + x * zdist * ooz * 2);
     yproj = (int)(HEIGHT / 2 + y * zdist * ooz);
     screenpos = xproj + yproj * WIDTH;
@@ -143,12 +152,28 @@ void calculatePoint(int i, int j, int k, int sym, char *color)
         {
             zbuffer[screenpos] = ooz;
             buffer[screenpos] = map(ooz);
-            colorbuffer[screenpos] = color;
+            colorbuffer[screenpos] = ColorMap(ooz);
         }
     }
 }
 
 char map(float a)
 {
-    return (char)(a*1000 + 66);
+    return (char)(a * 1000 + 66);
+}
+
+RGB ColorMap(float a)
+{
+
+    float shade = a * 1000;
+    int r, g = 0, b = 0;
+
+    // Color swap
+    r = (int)shade * 10;
+    if (r > 255)
+        r = 255;
+    else if (r < 0)
+        r = 0;
+
+    return (RGB){r, g, b};
 }
